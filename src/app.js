71a3da687c40043863843c1d361d8eead78c9f43
Roney-Juma <ochirone10@gmail.com>
2026-06-13 16:08:08 +0300

    prot

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI).then(() => {
   logger.info('Connected to MongoDB');
 })
