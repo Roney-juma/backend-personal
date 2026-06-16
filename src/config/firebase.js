a1ec82c10b74031c49194308f7130b0459938211
Roney-Juma <ochirone10@gmail.com>
2026-06-16 13:03:31 +0300

    Error

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
+const { getApps, initializeApp, cert } = require('firebase-admin/app');
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
