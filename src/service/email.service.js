55a31db722e5ad351b0f30c5b0724e3b9b9ef27d
Roney-Juma <ochirone10@gmail.com>
2026-06-27 17:15:45 +0300

    email

--- src/service/email.service.js
+++ src/service/email.service.js
@@ -12,12 +12,17 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const logEmailError = (label, to, error) => {
+  logger.error('%s | to=%s | message=%s | code=%s | responseCode=%s | response=%s',
+    label, to, error.message, error.code || 'N/A', error.responseCode || 'N/A', error.response || 'N/A');
+};
+
 const sendEmailNotification = (to, subject, text) => {
   const mailOptions = { from: process.env.EMAIL_HOST_USER, to, subject, text };
   return transporter.sendMail(mailOptions).then((info) => {
     logger.info('Email sent to %s: %s', to, info.response);
   }).catch((error) => {
-    logger.error('Error sending email to %s: %s', to, error.message);
+    logEmailError('Error sending email', to, error);
   });
 };
 
@@ -32,7 +37,7 @@ const sendInvoiceEmail = (to, subject, text, pdfBuffer, filename) => {
   return transporter.sendMail(mailOptions).then((info) => {
     logger.info('Invoice email sent to %s: %s', to, info.response);
   }).catch((error) => {
-    logger.error('Error sending invoice email to %s: %s', to, error.message);
+    logEmailError('Error sending invoice email', to, error);
   });
 };
 
